48368a91dc497be699fbf0d348e8e7f5019833e1
Y0USEFH <83236244+Y0USEF-H@users.noreply.github.com>
2021-06-10 17:40:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+""""
+a=input("type numbers here ")
+b=a.split()
+print(b)
+"""
+""""
+a=input("type numbers here ")
+b=a.split()
+print(b[1::2])
+"""
+
+""""
+a=int(input("type your first number "))
+b=int(input("type your second number "))
+def adding_num(a,b):
+return a + b
+sum=adding_num(a,b)
+print(sum)
+"""
+
+
+""""
+a=int(input("type here "))
+def nums():
+  if a==0:
+    print("1")
+  elif a==1:
+    print("0")
+  elif a==2:
+    print("0")
+  elif a==3:
+    print("0")
+  elif a==4:
+    print("0")
+  elif a==5:
+    print("0")
+  elif a==6:
+    print("1")
+  elif a==7:
+    print("0")
+  elif a==8:
+    print("2")
+  elif a==9:
+   print("1")  
+print(nums)
+"""
+
+ 
+
